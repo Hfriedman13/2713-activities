f516d31c60d490cea26fb994d79e52902bc8aa86
Hannah Friedman <hzf0023@auburn.edu>
2020-08-27 16:10:02 -0700

    wrote 3 methods with returning values, button retrieves values respectfully

--- HospitalCharges/HospitalCharges/Form1.cs
+++ HospitalCharges/HospitalCharges/Form1.cs
@@ -19,7 +19,7 @@ namespace HospitalCharges
 
         private double CalcStayCharges(double days)
         {
-            return (days * 350.0);
+            return days * 350;
         }
 
         private double CalcMiscCharges(double med, double surg, double lab, double rehab)
@@ -30,23 +30,39 @@ namespace HospitalCharges
 
         private double CalcTotalCharges(double days, double med, double surg, double lab, double rehab)
         {
-            return (CalcStayCharges(days) + CalcTotalCharges(days, med, surg, lab, rehab));
+            return (CalcStayCharges(days) + CalcMiscCharges(med, surg, lab, rehab));
         }
 
         private void stayCharge_Click(object sender, EventArgs e)
         {
-           (CalcStayCharges(Double.Parse(days.Text)));
-           
+          MessageBox.Show(CalcStayCharges(Double.Parse(days.Text)).ToString());
+            
         }
 
         private void MiscCharges_Click(object sender, EventArgs e)
         {
+            double medInput = Double.Parse(med.Text);
+            double surgInput = Double.Parse(surg.Text);
+            double labInput = Double.Parse(lab.Text);
+            double rehabInput = Double.Parse(rehab.Text);
+
+            double miscTotal = CalcMiscCharges(medInput, surgInput, labInput, rehabInput);
+
+            MessageBox.Show(miscTotal.ToString());
 
         }
 
         private void totalCharges_Click(object sender, EventArgs e)
         {
+            double daysInput = Double.Parse(days.Text);
+            double medInput = Double.Parse(med.Text);
+            double surgInput = Double.Parse(surg.Text);
+            double labInput = Double.Parse(lab.Text);
+            double rehabInput = Double.Parse(rehab.Text);
 
+            double totalCharge = CalcTotalCharges(daysInput, medInput, surgInput, labInput, rehabInput);
+
+            MessageBox.Show(totalCharge.ToString());
         }
     }
 }
